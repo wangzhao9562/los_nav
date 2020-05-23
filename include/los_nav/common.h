01d256b1c73ab49a3287a98f43814ca3fb91f078
wangzhao9562 <wangzhao9562@gmail.com>
2020-05-23 21:39:48 +0800

    Add copy constructor for CLine

--- include/los_nav/common.h
+++ include/los_nav/common.h
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-07 15:08:09
- * @LastEditTime: 2020-05-13 13:00:40
+ * @LastEditTime: 2020-05-23 21:18:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /los_nav/include/los_nav/common.h
@@ -35,17 +35,28 @@ namespace los_nav{
     struct CLine{
         CLine(){};
         CLine(double s_x, double s_y, double e_x, double e_y, 
-            double k, double b, bool is_reverse = false) : start_x_(s_x), start_y_(s_y), end_x_(e_x), end_y_(e_y), is_reverse_(is_reverse){}
-        double start_x_{0.0}, start_y_{0.0}, end_x_{0.0}, end_y_{0.0};
-        double k_{0.0}, b_{0.0};
-        bool is_reverse_{false};
+            double k, double b, bool is_reverse = false) : start_x_(s_x), start_y_(s_y), end_x_(e_x), end_y_(e_y), 
+                k_(k), b_(b), is_reverse_(is_reverse){}
+        CLine(const CLine& cline){
+            start_x_ = cline.start_x_;
+            start_y_ = cline.start_y_;
+            end_x_ = cline.end_x_;
+            end_y_ = cline.end_y_;
+            k_ = cline.k_;
+            b_ = cline.b_;
+            is_reverse_ = cline.is_reverse_;
+        }
+    
+        double start_x_, start_y_, end_x_, end_y_;
+        double k_, b_;
+        bool is_reverse_;
     };
 
     struct Circle{
         Circle(){};
         Circle(double origin_x, double origin_y, double r) : origin_x_(origin_x), origin_y_(origin_y), r_(r){}
-        double origin_x_{0.0}, origin_y_{0.0};
-        double r_{0.0};
+        double origin_x_, origin_y_;
+        double r_;
     };
 
     const double PI = 3.1415926535;
